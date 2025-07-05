fe2efab06b9dc2feffd704811d0676dea0c92458
EvoLib <evolib@dismail.de>
2025-07-05 19:31:38 +0200

    Refactor: Update example

--- examples/03_analysis/02_plotting.py
+++ examples/03_analysis/02_plotting.py
@@ -9,52 +9,36 @@ It demonstrates how to:
 - Interpret trends using matplotlib
 """
 
-import random
-
 from evolib import (
     Indiv,
-    MutationParams,
     Pop,
     Strategy,
     evolve_mu_lambda,
     mse_loss,
-    mutate_gauss,
     simple_quadratic,
 )
-from evolib.utils.plotting import (
-    plot_fitness,
-)
+
+from evolib.utils.plotting import plot_fitness
 
 
 def my_fitness(indiv: Indiv) -> None:
     expected = 0.0
-    predicted = simple_quadratic(indiv.para)
+    predicted = simple_quadratic(indiv.para.vector)
     indiv.fitness = mse_loss(expected, predicted)
-
-
-def my_mutation(indiv: Indiv, params: MutationParams) -> None:
-    indiv.para = mutate_gauss(indiv.para, params.strength, params.bounds)
-
-
-def initialize_population(pop: Pop) -> None:
-    for _ in range(pop.parent_pool_size):
-        new_indiv = pop.create_indiv()
-        new_indiv.para = random.uniform(-3, 3)
-        pop.add_indiv(new_indiv)
-    for indiv in pop.indivs:
-        my_fitness(indiv)
 
 
 # Setup
 my_pop = Pop(config_path="population.yaml")
-initialize_population(my_pop)
+my_pop.initialize_population()
+my_pop.set_functions(fitness_function=my_fitness)
 
 # Evolution
 for _ in range(my_pop.max_generations):
-    evolve_mu_lambda(my_pop, my_fitness, my_mutation, strategy=Strategy.MU_PLUS_LAMBDA)
+    evolve_mu_lambda(my_pop, strategy=Strategy.MU_PLUS_LAMBDA)
 
 # History to DataFrame
 history = my_pop.history_logger.to_dataframe()
 
 # Plotting
 plot_fitness(history, show=True, save_path="./figures/02_plotting.png")
+print("Plot saved to ./figures/02_plotting.png.")
