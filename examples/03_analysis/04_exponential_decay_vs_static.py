e306abb3ad65a5089f985250ddb228f29717a68b
EvoLib <evolib@dismail.de>
2025-07-05 19:49:06 +0200

    Refactor: Update example

--- examples/03_analysis/04_exponential_decay_vs_static.py
+++ examples/03_analysis/04_exponential_decay_vs_static.py
@@ -18,12 +18,10 @@ import pandas as pd
 
 from evolib import (
     Indiv,
-    MutationParams,
     Pop,
     Strategy,
     evolve_mu_lambda,
     mse_loss,
-    mutate_gauss,
     plot_fitness_comparison,
     rosenbrock,
 )
@@ -32,31 +30,17 @@ from evolib import (
 # User-defined fitness function
 def my_fitness(indiv: Indiv) -> None:
     expected = [1.0, 1.0, 1.0, 1.0]
-    predicted = rosenbrock(indiv.para)
+    predicted = rosenbrock(indiv.para.vector)
     indiv.fitness = mse_loss(expected, predicted)
-
-
-# User-defined mutation function
-def my_mutation(indiv: Indiv, params: MutationParams) -> None:
-    for idx, _ in enumerate(indiv.para):
-        indiv.para[idx] = mutate_gauss(indiv.para[idx], params.strength, params.bounds)
-
-
-def initialize_population(pop: Pop) -> None:
-    for _ in range(pop.parent_pool_size):
-        my_indiv = pop.create_indiv()
-        my_indiv.para = [-2.0, 2.0, -1.5, 1.5]  # for better comparison reasons
-        pop.add_indiv(my_indiv)
-    for indiv in pop.indivs:
-        my_fitness(indiv)
 
 
 def run_experiment(config_path: str) -> pd.DataFrame:
     pop = Pop(config_path)
-    initialize_population(pop)
+    pop.initialize_population()
+    pop.set_functions(fitness_function=my_fitness)
 
     for _ in range(pop.max_generations):
-        evolve_mu_lambda(pop, my_fitness, my_mutation, strategy=Strategy.MU_PLUS_LAMBDA)
+        evolve_mu_lambda(pop, strategy=Strategy.MU_PLUS_LAMBDA)
 
     history = pop.history_logger.to_dataframe()
     print(history)
