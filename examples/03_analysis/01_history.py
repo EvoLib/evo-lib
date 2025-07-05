976a060c6bde277dac3dbc8fb49dbadc0da669f6
EvoLib <evolib@dismail.de>
2025-07-05 18:41:11 +0200

    Refactor: Update example

--- examples/03_analysis/01_history.py
+++ examples/03_analysis/01_history.py
@@ -8,73 +8,45 @@ It introduces:
 - Accessing and printing the history as a DataFrame
 
 Requirements:
-- 'population.yaml' and 'individual.yaml' must be present in the
-current working directory
+- 'population.yaml' must be present in the current working directory
 """
-
-import random
 
 from evolib import (
     Indiv,
-    MutationParams,
     Pop,
     Strategy,
     evolve_mu_lambda,
     mse_loss,
-    mutate_gauss,
-    simple_quadratic,
+    simple_quadratic
 )
 
 
 def my_fitness(indiv: Indiv) -> None:
     """Simple fitness function using the quadratic benchmark and MSE loss."""
     expected = 0.0
-    predicted = simple_quadratic(indiv.para)
+    predicted = simple_quadratic(indiv.para.vector)
     indiv.fitness = mse_loss(expected, predicted)
-
-
-def my_mutation(indiv: Indiv, params: MutationParams) -> None:
-    """
-    Mutates the individual's parameters using Gaussian mutation.
-
-    Args:
-        indiv (Indiv): The individual to mutate.
-        params (MutationParams): Mutation context object that includes:
-            - strength (float): Standard deviation of the Gaussian mutation.
-            - bounds (tuple[float, float]): Value limits for the mutation.
-            - rate (Optional[float]): (Unused) mutation rate, if applicable.
-            - bias (Optional[float]): (Unused) bias mutation strength for
-              neural networks.
-    """
-    indiv.para = mutate_gauss(indiv.para, params.strength, params.bounds)
 
 
 def print_population(pop: Pop, title: str) -> None:
     print(f"{title}")
-    for i, print_indiv in enumerate(pop.indivs):
+    for i, indiv in enumerate(pop.indivs):
         print(
-            f"  Indiv {i}: Parameter = {indiv.para:.4f},"
-            "Fitness = {indiv.fitness:.6f}"
+            f"  Indiv {i}: Parameter = {indiv.para.vector},"
+            f"Fitness = {indiv.fitness:.6f}"
         )
 
 
 # Load configuration and initialize population
 my_pop = Pop(config_path="population.yaml")
-
-for _ in range(my_pop.parent_pool_size):
-    new_indiv = my_pop.create_indiv()
-    new_indiv.para = random.uniform(-3, 3)  # Scalar parameter
-    my_pop.add_indiv(new_indiv)
-
-
-for indiv in my_pop.indivs:
-    my_fitness(indiv)
+my_pop.initialize_population()
+my_pop.set_functions(fitness_function=my_fitness)
 
 print_population(my_pop, "Initial Parents")
 
 # Mu Plus Lambda
 for gen in range(my_pop.max_generations):
-    evolve_mu_lambda(my_pop, my_fitness, my_mutation, strategy=Strategy.MU_PLUS_LAMBDA)
+    evolve_mu_lambda(my_pop, strategy=Strategy.MU_PLUS_LAMBDA)
 
 history = my_pop.history_logger.to_dataframe()
 print(
