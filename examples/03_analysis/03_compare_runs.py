4d957e4b2730c88cbbda832b4f1882f6a4928a53
EvoLib <evolib@dismail.de>
2025-07-05 19:42:06 +0200

    Refactor: Update example

--- examples/03_analysis/03_compare_runs.py
+++ examples/03_analysis/03_compare_runs.py
@@ -9,12 +9,10 @@ import pandas as pd
 
 from evolib import (
     Indiv,
-    MutationParams,
     Pop,
     Strategy,
     evolve_mu_lambda,
     mse_loss,
-    mutate_gauss,
     simple_quadratic,
 )
 from evolib.utils.plotting import plot_fitness_comparison
@@ -22,31 +20,19 @@ from evolib.utils.plotting import plot_fitness_comparison
 
 def my_fitness(indiv: Indiv) -> None:
     expected = 0.0
-    predicted = simple_quadratic(indiv.para)
+    predicted = simple_quadratic(indiv.para.vector)
     indiv.fitness = mse_loss(expected, predicted)
-
-
-def my_mutation(indiv: Indiv, params: MutationParams) -> None:
-    indiv.para = mutate_gauss(indiv.para, params.strength, params.bounds)
-
-
-def initialize_population(pop: Pop) -> None:
-    for _ in range(pop.parent_pool_size):
-        new_indiv = pop.create_indiv()
-        new_indiv.para = 3  # for better comparison reasons
-        pop.add_indiv(new_indiv)
-    for indiv in pop.indivs:
-        my_fitness(indiv)
 
 
 def run_experiment(mutation_strength: float) -> pd.DataFrame:
     pop = Pop(config_path="population.yaml")
-    initialize_population(pop)
+    pop.initialize_population()
+    pop.set_functions(fitness_function=my_fitness)
 
     pop.mutation_strength = mutation_strength
 
     for _ in range(pop.max_generations):
-        evolve_mu_lambda(pop, my_fitness, my_mutation, strategy=Strategy.MU_PLUS_LAMBDA)
+        evolve_mu_lambda(pop, strategy=Strategy.MU_PLUS_LAMBDA)
 
     return pop.history_logger.to_dataframe()
 
