f01387f15711f35bb2c750e2e0fe06f2fbccee5c
EvoLib <evolib@dismail.de>
2025-07-05 19:54:00 +0200

    Refactor: Update example

--- examples/03_analysis/05_adaptive_global_vs_static.py
+++ examples/03_analysis/05_adaptive_global_vs_static.py
@@ -13,9 +13,6 @@ Key Elements:
 - The experiment compares static vs. adaptive mutation rate strategies.
 - Results are visualized using a fitness comparison plot across generations.
 
-Requirements:
-- 'population.yaml' and 'individual.yaml' must be present in the working directory.
-- Mutation strategy is specified via the configuration file.
 - Results are saved to './figures/05_adaptive_global.png'
 """
 
@@ -23,12 +20,10 @@ import pandas as pd
 
 from evolib import (
     Indiv,
-    MutationParams,
     Pop,
     Strategy,
     evolve_mu_lambda,
     mse_loss,
-    mutate_gauss,
     plot_fitness_comparison,
     rosenbrock,
 )
@@ -37,33 +32,19 @@ from evolib import (
 # User-defined fitness function
 def my_fitness(indiv: Indiv) -> None:
     expected = [1.0, 1.0, 1.0, 1.0]
-    predicted = rosenbrock(indiv.para)
+    predicted = rosenbrock(indiv.para.vector)
     indiv.fitness = mse_loss(expected, predicted)
-
-
-# User-defined mutation function
-def my_mutation(indiv: Indiv, params: MutationParams) -> None:
-    for idx, _ in enumerate(indiv.para):
-        indiv.para[idx] = mutate_gauss(indiv.para[idx], params.strength, params.bounds)
-
-
-def initialize_population(init_pop: Pop) -> None:
-    for _ in range(init_pop.parent_pool_size):
-        new_indiv = init_pop.create_indiv()
-        new_indiv.para = [-2.0, 2.0, -1.5, 1.5]  # for better comparison reasons
-        init_pop.add_indiv(new_indiv)
-    for indiv in init_pop.indivs:
-        my_fitness(indiv)
 
 
 def run_experiment(config_path: str) -> pd.DataFrame:
     pop = Pop(config_path)
-    initialize_population(pop)
+    pop.initialize_population()
+    pop.set_functions(fitness_function=my_fitness)
 
     for _ in range(pop.max_generations):
-        evolve_mu_lambda(pop, my_fitness, my_mutation, strategy=Strategy.MU_PLUS_LAMBDA)
+        evolve_mu_lambda(pop, strategy=Strategy.MU_PLUS_LAMBDA)
 
-    pd.set_option("display.max_rows", None)  # Alle Zeilen anzeigen
+    pd.set_option("display.max_rows", None)
     history = pop.history_logger.to_dataframe()
     print(history)
 
