efffd70886f272c9eb09017b606d88c4d9572abe
EvoLib <evolib@dismail.de>
2025-07-20 10:36:55 +0200

    Bump Version

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "EvoLib"
 author = "EvoLib"
 copyright = "2025, EvoLib"
-release = "0.1.0adev10"
+release = "0.1.0adev11"
 
 extensions = [
     "sphinx.ext.autodoc",
